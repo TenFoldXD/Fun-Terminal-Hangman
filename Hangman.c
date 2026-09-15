956532bc2032bcdb9e9b17b1437f29ce1affc2cb
TenFold <lucayang550@proton.me>
2026-09-15 18:03:07 +0000

    Is this gonna log my actions every single time?

--- Hangman.c
+++ Hangman.c
@@ -110,7 +110,7 @@ void hang(char word[], int lettercount){
         }
     }
 
-    printf("Now... GET OUUUUUTTTT! (baldi reference)\nThe word was %s\n",word);
+    printf("You lost the game!\nThe word was %s\n",word);
     exit(1);
 }
 int main(){
@@ -120,7 +120,7 @@ int main(){
     char choice;
     char *wordlist[] = {"elephant","ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf", "wombat", "zebra"};
 
-    printf("Choice '!' to quit, and '$' to begin a new game!\n");
+    printf("Choose '!' to quit, or '$' to begin a new game!\n");
     while (true){
 
         scanf("%c", &choice);
