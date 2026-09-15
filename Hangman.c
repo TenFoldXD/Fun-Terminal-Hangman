2a5d0bce99cfd4d8a53f4ca57bf9a14391ae0796
TenFold <lucayang550@proton.me>
2026-09-15 18:04:38 +0000

    Refactor wordlist initialization in Hangman.c
    Updated wordlist declaration for clarity and maintainability.

--- Hangman.c
+++ Hangman.c
@@ -118,7 +118,7 @@ int main(){
     srand(time(NULL));
 
     char choice;
-    char *wordlist[] = {"elephant","ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf", "wombat", "zebra"};
+    char *wordlist[] = {"elephant","ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf", "wombat", "zebra"}; // You can add more words here if you'd like!
 
     printf("Choose '!' to quit, or '$' to begin a new game!\n");
     while (true){
